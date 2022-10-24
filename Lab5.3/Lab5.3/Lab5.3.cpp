9284419867ec5038a81c42775eb445afbca64d0d
arturjkeee <artur.stepanyan.03@mail.ru>
2022-10-24 11:57:38 +0300

    Lab 5.3

---
+++ Lab5.3/Lab5.3/Lab5.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "WeaponClass.h"
+int main()
+{
+    //WeaponClass sword("Sword",10,2,ONEHANDED); 
+    MagicWeaponClass staff ("FirstStaff" , 10,2,TWOHANDED,3);
+    staff.Attack();
+   
+    cout << "\n\n";
+
+    SingleUsedWeapon knife ("FirstKnife", 5, 1, ONEHANDED,false);
+    knife.Attack();
+    
+    cout << "\n\n";
+   
+    knife.Attack();
+    
+    cout << "\n\n";
+
+    WeaponHand<bool> someWeapon(0, 1);
+    cout << someWeapon.getIsLeft() << " " << someWeapon.getIsRight() << endl;
+    WeaponHand<string> anotherWeapon("Is lefthand", "Is righthand");
+    cout << anotherWeapon.getIsLeft() << " " << anotherWeapon.getIsRight() << endl;
+}
