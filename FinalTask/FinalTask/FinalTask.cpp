79128d03d9c7fd432da16c25146e491af2ea3619
arturjkeee <artur.stepanyan.03@mail.ru>
2022-12-29 09:26:10 +0300

    FinalTask

---
+++ FinalTask/FinalTask/FinalTask.cpp
@@ -0,0 +1,234 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class BST
+{
+public:
+    struct node
+    {
+        int data;
+        node* left;
+        node* right;
+        int height;
+    };
+
+    node* root;
+
+    void makeEmpty(node* t)
+    {
+        if (t == NULL)
+            return;
+        makeEmpty(t->left);
+        makeEmpty(t->right);
+        delete t;
+    }
+
+    node* insert(int x, node* t)
+    {
+        if (t == NULL)
+        {
+            t = new node;
+            t->data = x;
+            t->height = 0;
+            t->left = t->right = NULL;
+        }
+        else if (x < t->data)
+        {
+            t->left = insert(x, t->left);
+            if (height(t->left) - height(t->right) == 2)
+            {
+                if (x < t->left->data)
+                    t = singleRightRotate(t);
+                else
+                    t = doubleRightRotate(t);
+            }
+        }
+        else if (x > t->data)
+        {
+            t->right = insert(x, t->right);
+            if (height(t->right) - height(t->left) == 2)
+            {
+                if (x > t->right->data)
+                    t = singleLeftRotate(t);
+                else
+                    t = doubleLeftRotate(t);
+            }
+        }
+
+        t->height = max(height(t->left), height(t->right)) + 1;
+        return t;
+    }
+
+    virtual node* singleRightRotate(node*& t) = 0;
+
+    virtual node* singleLeftRotate(node*& t) = 0;
+
+    virtual node* doubleLeftRotate(node*& t) = 0;
+
+    virtual node* doubleRightRotate(node*& t) = 0;
+
+    node* findMin(node* t)
+    {
+        if (t == NULL)
+            return NULL;
+        else if (t->left == NULL)
+            return t;
+        else
+            return findMin(t->left);
+    }
+
+    node* findMax(node* t)
+    {
+        if (t == NULL)
+            return NULL;
+        else if (t->right == NULL)
+            return t;
+        else
+            return findMax(t->right);
+    }
+
+    node* remove(int x, node* t)
+    {
+        node* temp;
+
+        // Element not found
+        if (t == NULL)
+            return NULL;
+
+        // Searching for element
+        else if (x < t->data)
+            t->left = remove(x, t->left);
+        else if (x > t->data)
+            t->right = remove(x, t->right);
+
+        else if (t->left && t->right)
+        {
+            temp = findMin(t->right);
+            t->data = temp->data;
+            t->right = remove(t->data, t->right);
+        }
+        else
+        {
+            temp = t;
+            if (t->left == NULL)
+                t = t->right;
+            else if (t->right == NULL)
+                t = t->left;
+            delete temp;
+        }
+        if (t == NULL)
+            return t;
+
+        t->height = max(height(t->left), height(t->right)) + 1;
+
+        if (height(t->left) - height(t->right) == 2)
+        {
+            // right right case
+            if (height(t->left->left) - height(t->left->right) == 1)
+                return singleLeftRotate(t);
+            // right left case
+            else
+                return doubleLeftRotate(t);
+        }
+        else if (height(t->right) - height(t->left) == 2)
+        {
+            if (height(t->right->right) - height(t->right->left) == 1)
+                return singleRightRotate(t);
+            else
+                return doubleRightRotate(t);
+        }
+        return t;
+    }
+
+    int height(node* t)
+    {
+        return (t == NULL ? -1 : t->height);
+    }
+
+    int getBalance(node* t)
+    {
+        if (t == NULL)
+            return 0;
+        else
+            return height(t->left) - height(t->right);
+    }
+
+    void inorder(node* t)
+    {
+        if (t == NULL)
+            return;
+        inorder(t->left);
+        cout << t->data << " ";
+        inorder(t->right);
+    }
+
+    BST()
+    {
+        root = NULL;
+    }
+
+    void insert(int x)
+    {
+        root = insert(x, root);
+    }
+
+    void remove(int x)
+    {
+        root = remove(x, root);
+    }
+
+    void display()
+    {
+        inorder(root);
+        cout << endl;
+    }
+};
+class AVL : public BST
+{
+    node* singleRightRotate(node*& t)
+    {
+        node* u = t->left;
+        t->left = u->right;
+        u->right = t;
+        t->height = max(height(t->left), height(t->right)) + 1;
+        u->height = max(height(u->left), t->height) + 1;
+        return u;
+    }
+
+    node* singleLeftRotate(node*& t)
+    {
+        node* u = t->right;
+        t->right = u->left;
+        u->left = t;
+        t->height = max(height(t->left), height(t->right)) + 1;
+        u->height = max(height(t->right), t->height) + 1;
+        return u;
+    }
+
+    node* doubleLeftRotate(node*& t)
+    {
+        t->right = singleRightRotate(t->right);
+        return singleLeftRotate(t);
+    }
+
+    node* doubleRightRotate(node*& t)
+    {
+        t->left = singleLeftRotate(t->left);
+        return singleRightRotate(t);
+    }
+};
+int main()
+{
+    cout << "Enter the number of elements: " << endl;
+    int n; cin >> n;
+    AVL t;
+    cout << "Enter the elements: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        int x; cin >> x;
+        t.insert(x);
+    }
+    t.display();
+}
+ 
